870895f00d0b19cbb2375522b633df30f0969e4f
Lee <wlee2019@wlee2019-mac.attlocal.net>
2016-12-21 18:29:36 -0600

    images update on new search

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,24 +1,28 @@
+var firstClick = false;
 $('#button').click(function(e) {
-  e.preventDefault()
-  var obj = {q: $('#search').val()}
+  e.preventDefault();
+  var obj = {q: $('#search').val()};
   $.ajax({
     url: "http://api.giphy.com/v1/gifs/search?"+$.param(obj)+"&api_key=dc6zaTOxFJmzC",
   })
     .done(function(data) {
-      if ( console && console.log ) {
-      	for (var i = 0; i < 25; i++)
+      if (console && console.log) {
+        if (firstClick === true)
+        {
+          for (var y = 0; y < 25; y++)
+          {
+            $('#'+y).remove();
+          }
+        }
+        firstClick = true;
+      	for (var i = 0; i < 25; i++) // this for-loop creates a clone of class "hidden", adds the source, and clones it
       	{
       		var clone = $('div.hidden').clone();
-      		$("#source").attr('src', data.data[i].images.fixed_height.url)
-      		clone.removeClass("hidden").addClass("col s6 m4")
-          $('div.row_1').append(clone)
+          $(clone).attr('id', i);
+      		$("#source").attr('src', data.data[i].images.fixed_height.url);
+      		clone.removeClass("hidden").addClass("col s6 m4");
+          $('div.row_1').append(clone);
       	}
-        //loop over first 3 in data.data
-          //clone hidden div (column)
-          //set i.images.fixed_height.url as src property of image
-          // remove the "hidden" class in the clone
-          //add it to page
-
       }
-    });
+    })
 })
